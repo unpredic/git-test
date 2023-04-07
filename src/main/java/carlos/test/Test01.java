d56792990baea8560aa734efcfa3db7204e743af
upredic <45909514+unpredic@users.noreply.github.com>
2023-04-07 19:49:11 +0800

    pull test

--- src/main/java/carlos/test/Test01.java
+++ src/main/java/carlos/test/Test01.java
@@ -5,5 +5,6 @@ public class Test01 {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello push");
+        System.out.println("hello pull");
     }
 }
