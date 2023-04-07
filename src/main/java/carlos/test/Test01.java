e0fc74cb9b9343caac41548d4441cac4506d68c3
upredic <unpredic326@qq.com>
2023-04-07 19:46:09 +0800

    third push commit

--- src/main/java/carlos/test/Test01.java
+++ src/main/java/carlos/test/Test01.java
@@ -4,5 +4,6 @@ public class Test01 {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git");
+        System.out.println("hello push");
     }
 }
