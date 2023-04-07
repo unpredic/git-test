81c5d19b81162f83f7f180f64f5f513319309873
upredic <unpredic326@qq.com>
2023-04-07 19:34:58 +0800

    second attempt commit

--- src/main/java/carlos/test/Test01.java
+++ src/main/java/carlos/test/Test01.java
@@ -3,5 +3,6 @@ package carlos.test;
 public class Test01 {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git");
     }
 }
